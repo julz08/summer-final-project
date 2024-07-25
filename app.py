39606a0d05c635261558dc3afb880d80c10a8a38
Juliana <juliana.li@csedge.org>
2024-07-25 11:58:34 -0700

    added about me website

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
+@app.route('/about-me')
+def homepage():
+    return render_template("about-me.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
