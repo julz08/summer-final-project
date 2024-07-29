a456854abb9e85c00b3a450f6275a79648a679a3
Juliana <juliana.li@csedge.org>
2024-07-29 11:07:58 -0700

    added app routes and changed href

--- app.py
+++ app.py
@@ -8,7 +8,43 @@ def homepage():
 
 @app.route('/about-me')
 def aboutme():
-    return render_template("about-me.html")
+    return render_template("wip.html")
+
+@app.route('/archive-vids')
+def archivevid():
+    return render_template("wip.html")
+
+@app.route('/archive-media')
+def archivemedia():
+    return render_template("wip.html")
+
+@app.route('/archive-github')
+def archivegithub():
+    return render_template("wip.html")
+
+@app.route('/review-manga')
+def reviewmanga():
+    return render_template("wip.html")
+
+@app.route('/review-webtoons')
+def reviewwebtoons():
+    return render_template("wip.html")
+
+@app.route('/review-manhwa')
+def reviewmanhwa():
+    return render_template("wip.html")
+
+@app.route('/review-books')
+def reviewbooks():
+    return render_template("wip.html")
+
+@app.route('/review-shows')
+def reviewshows():
+    return render_template("wip.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("wip.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
